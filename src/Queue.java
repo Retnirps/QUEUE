80c05208cd21f5f631ee899040c5147009b91261
Retnirps <Vasiliy23898@gmail.com>
2020-04-13 20:57:51 +0300

    Interface created

--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,11 @@
 public interface Queue {
+    void enqueue(Object object);
+    Object element();
+    Object dequeue();
+    int size();
+    boolean isEmpty();
+    void clear();
+
+    Object[] toArray();
 }
+
